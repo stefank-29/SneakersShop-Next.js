c2aea49504b452d7875967a88e7af95db11a5935
stefank-29 <stefankaraferovic@gmail.com>
2021-06-09 10:42:09 +0200

    progress bar

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,13 @@
 // import '../styles/globals.css';
+import NProgress from 'nprogress';
 import Page from '../components/Page';
+import Router from 'next/router';
 import { CartStateProvider } from '../lib/cartState';
+import '../styles/nprogress.css';
+
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
 function MyApp({ Component, pageProps }) {
     return (
